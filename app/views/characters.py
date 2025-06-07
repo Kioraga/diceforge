f729e6946d8b1b8854164fb275a34106c9ed243d
Daniel Cancelas <dani12cg@gmail.com>
2025-06-07 17:40:30 +0200

    Implement save bonus update functionality and refactor plugin stats retrieval

--- app/views/characters.py
+++ app/views/characters.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 from pydantic import ValidationError
 
-from app.models import Character, User
+from app.models import Character
 from app.plugins import compendium
 
 
